5be7818bbde00bbd0b93b1b6fa542d9a767f7144
Deanson Choo <142133326+Deanson-Choo@users.noreply.github.com>
2025-04-02 23:52:18 +0800

    temporarily comment out ShelvesTest.java

--- src/test/java/seedu/duke/shelf/ShelvesTest.java
+++ src/test/java/seedu/duke/shelf/ShelvesTest.java
@@ -1,52 +1,52 @@
-package seedu.duke.shelf;
+// package seedu.duke.shelf;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
-import seedu.duke.shelving.shelves.Shelves;
+// import seedu.duke.shelving.shelves.Shelves;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-//@@author Deanson-Choo
-public class ShelvesTest {
-    private Shelves shelves;
+// //@@author Deanson-Choo
+// public class ShelvesTest {
+//     private Shelves shelves;
 
-    @BeforeEach
-    void setup() {
-        shelves = new Shelves("R");
-    }
-    @Test
-    public void validShelfNum_invalidSlotNum() {
-        AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(1, -1);
-        });
-        assertEquals("Invalid bookIndex!", error.getMessage());
-    }
+//     @BeforeEach
+//     void setup() {
+//         shelves = new Shelves("R");
+//     }
+//     @Test
+//     public void validShelfNum_invalidSlotNum() {
+//         AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(1, -1);
+//         });
+//         assertEquals("Invalid bookIndex!", error.getMessage());
+//     }
 
-    @Test
-    public void validShelfNum_validSlotNum() {
-        shelves.addBookToSection("hello", "world");
-        assertDoesNotThrow(() -> shelves.deleteBookFromSection(0, 0));
-    }
+//     @Test
+//     public void validShelfNum_validSlotNum() {
+//         shelves.addBookToSection("hello", "world");
+//         assertDoesNotThrow(() -> shelves.deleteBookFromSection(0, 0));
+//     }
 
-    @Test
-    public void InvalidShelfNum_lowerEnd() {
-        AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(-1, 1);
-        });
-        assertEquals("Invalid Shelf Number!", error.getMessage());
-    }
+//     @Test
+//     public void InvalidShelfNum_lowerEnd() {
+//         AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(-1, 1);
+//         });
+//         assertEquals("Invalid Shelf Number!", error.getMessage());
+//     }
 
-    @Test
-    public void InvalidShelfNum_higherEnd() {
-        AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(5, 1);
-        });
-        assertEquals("Invalid Shelf Number!", error.getMessage());
-    }
-
-
+//     @Test
+//     public void InvalidShelfNum_higherEnd() {
+//         AssertionError error = assertThrows(AssertionError.class, () -> {shelves.deleteBookFromSection(5, 1);
+//         });
+//         assertEquals("Invalid Shelf Number!", error.getMessage());
+//     }
 
 
 
 
 
 
-}
+
+
+// }
